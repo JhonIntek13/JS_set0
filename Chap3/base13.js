1c3f0e51c182810731d52ad076451c87bf25f9ce
JhonIntek13 <136015556+JhonIntek13@users.noreply.github.com>
2023-07-06 10:53:04 -0600

    fix base13

--- Chap3/base13.js
+++ Chap3/base13.js
@@ -1,20 +1,13 @@
 function multiplyInBase13(num1, num2) {
-  // Convert the numbers to base 10
-  const num1_base10 = parseInt(num1, 13);
-  const num2_base10 = parseInt(num2, 13);
-  
-  // Perform the multiplication in base 10
-  const result_base10 = num1_base10 * num2_base10;
-  
-  // Convert the result back to base 13
-  let result_base13 = '';
-  let quotient = result_base10;
-  
-  while (quotient > 0) {
-    const remainder = quotient % 13;
-    result_base13 = remainder.toString(13) + result_base13;
-    quotient = Math.floor(quotient / 13);
-  }
-  
+
+
+  // Perform the multiplication in base 10 without convert to base 10 they are already in this
+  const result_base10 = num1 * num2;
+
+  // Convert the result to base 13
+  const result_base13 = result_base10.toString(13);
+
   return result_base13;
 }
+
+console.log(multiplyInBase13(9,6));
